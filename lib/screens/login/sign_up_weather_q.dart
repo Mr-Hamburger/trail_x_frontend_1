0bde62e9ed836cac8f8111d4de6c908b104d6532
Terry Liu <terryliu9917@gmail.com>
2024-09-30 15:42:05 -0400

    Five Commit for Systematic Files 9_30_24

--- lib/screens/login/sign_up_weather_q.dart
+++ lib/screens/login/sign_up_weather_q.dart
@@ -10,52 +10,60 @@ class SignUpWeatherQ extends StatefulWidget {
 }
 
 class _SignUpWeatherQ extends State<SignUpWeatherQ> {
-  @override
-  void initState() {
-    super.initState();
-    // Initialize any data or perform actions before the page is built
-  }
-
-  @override
-  void dispose() {
-    // Clean up any resources or listeners when the widget is removed from the widget tree
-    super.dispose();
-  }
+  int selectedIndex = -1; // No selection initially
 
   @override
   Widget build(BuildContext context) {
-    int selectedIndex = 0;
     return Scaffold(
-        backgroundColor: backgroundColor,
-        body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: GridView.builder(
-              physics: const ClampingScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1),
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ElevatedButton(
-                  style: ButtonStyle(foregroundColor: ),
-                  onPressed: () => setState(() {
-                    selectedIndex = index;
-                  }),
-                  child: Text(
-                    "Sunny",
-                    style: GoogleFonts.getFont(
-                      'Inter',
-                      textStyle: TextStyle(
-                          color: loginTextColor,
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: 0.6,
-                          wordSpacing: 1,
-                          fontSize: 20),
+      backgroundColor: backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.all(22),
+        child: SizedBox(
+          height: 600,
+          child: GridView.builder(
+            physics: const ClampingScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1),
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              bool isSelected = selectedIndex == index;
+              return FilledButton(
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))),
+                  backgroundColor: MaterialStateProperty.all(
+                    isSelected
+                        ? loginGreenColor
+                        : backgroundColor, // Change button color based on selection
+                  ),
+                  foregroundColor: MaterialStateProperty.all(
+                    isSelected
+                        ? backgroundColor
+                        : loginGreenColor, // Change text color based on selection
+                  ),
+                ),
+                onPressed: () => setState(() {
+                  selectedIndex = index; // Update selected index
+                }),
+                child: Text(
+                  "Sunny", // You can change this to dynamic content based on index
+                  style: GoogleFonts.getFont(
+                    'Inter',
+                    textStyle: TextStyle(
+                      color: isSelected ? Colors.white : loginTextColor,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.6,
+                      wordSpacing: 1,
+                      fontSize: 20,
                     ),
                   ),
-                );
-              },
-            )));
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
-
-  // Define any other helper methods or callbacks
 }
